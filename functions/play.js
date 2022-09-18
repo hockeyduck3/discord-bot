57f26ec7242bb1c1bb842ac01b398079d451745a
LJ Spencer <hockeyduck3@gmail.com>
2022-09-17 20:50:06 -0600

    Added skip function

--- functions/play.js
+++ functions/play.js
@@ -4,12 +4,14 @@ const { MessageEmbed } = require('discord.js');
 
 let songArray = [];
 let songPlaying = false;
+let connection;
 
 module.exports = {
-    name: ['play', 'p'],
+    name: ['play', 'p', 'skip'],
     description: 'play song in discord channel',
     async execute(message, args) {
         const vc = message.member.voice.channel;
+        const command = message.content.substring(1).trim().split(/ +/).shift();
 
         if (!vc) {
             message.reply('You gotta be in a voice channel bro');
@@ -28,67 +30,80 @@ module.exports = {
                 return;
         }
 
-        if (!args.length) {
-            message.reply('You need to add another argument bro');
-            return;
-        }
+        if (command == 'play' || command == 'p') {
+            if (!args.length) {
+                message.reply('You need to add another argument bro');
+                return;
+            }
 
-        let incomingVideo = args.join(' ');
+            let incomingVideo = args.join(' ');
 
-        const findVideo = async (query) => {
-            const result = await ytsearch(query);
+            const findVideo = async (query) => {
+                const result = await ytsearch(query);
 
-            return (result.videos.length > 1) ? result.videos[0] : null;
-        }
+                return (result.videos.length > 1) ? result.videos[0] : null;
+            }
 
-        const video = await findVideo(incomingVideo);
+            const video = await findVideo(incomingVideo);
 
-        if (!songPlaying) {
-            var connection = await vc.join();
-            songPlaying = true;
-        } else {
-            songArray.push(video);
-            console.log(`songPlaying = ${songPlaying}`);
-            console.log(`songArray = ${songArray}`);
-            message.channel.send(`Added ${video.title} to the queue`);
-            return;
-        }
+            if (!songPlaying) {
+                connection = await vc.join();
+                songPlaying = true;
+            } else {
+                songArray.push(video);
+                console.log(`songPlaying = ${songPlaying}`);
+                console.log(`songArray = ${songArray}`);
+                message.channel.send(`Added ${video.title} to the queue`);
+                return;
+            }
 
-        const playSong = async(song) => {
-            const stream = ytdl(song.url, {filter: 'audioonly', quality: 'lowestaudio'}).on('error', err => {
-                console.log(err);
-                vc.leave();
-            });
-            connection.play(stream, {seek: 0, volume: 1})
-                .on('finish', () =>{
-                    if(songArray.length == 0) {
-                        vc.leave();
-                    } else {
-                        playSong(songArray[0]);
-                        songArray.shift();
-                        console.log(songArray == 0);
-                    }
+            const playSong = async(song) => {
+                const stream = ytdl(song.url, {filter: 'audioonly', quality: 'lowestaudio'}).on('error', err => {
+                    console.log(err);
+                    vc.leave();
                 });
-    
-                const nowPlaying = new MessageEmbed()
-                    .setColor([255, 0, 255])
-                    .setAuthor('Util Music')
-                    .addFields(
-                        { name: `Playing ${song.title}`, value: `${song.url}` },
-                        { name:'\u200B', value: '\u200B' },
-                    )
-                    .setThumbnail(song.image)
-                    .setTimestamp()
-                    .setFooter(`${message.author.username}`, message.author.avatarURL({ dynamic:true }));
+                connection.play(stream, {seek: 0, volume: 1})
+                    .on('finish', () =>{
+                        if(songArray.length == 0) {
+                            vc.leave();
+                        } else {
+                            playSong(songArray[0]);
+                            songArray.shift();
+                            console.log(songArray == 0);
+                        }
+                    });
         
-            await message.channel.send(nowPlaying);
+                    const nowPlaying = new MessageEmbed()
+                        .setColor([255, 0, 255])
+                        .setAuthor('Util Music')
+                        .addFields(
+                            { name: `Playing ${song.title}`, value: `${song.url}` },
+                            { name:'\u200B', value: '\u200B' },
+                        )
+                        .setThumbnail(song.image)
+                        .setTimestamp()
+                        .setFooter(`${message.author.username}`, message.author.avatarURL({ dynamic:true }));
+            
+                await message.channel.send(nowPlaying);
+            }
+
+            if (video) {
+                playSong(video);
+            } else {
+                message.reply(`I couldn't find a video for that fam`);
+                return;
+            }
         }
 
-        if (video) {
-            playSong(video);
-        } else {
-            message.reply(`I couldn't find a video for that fam`);
-            return;
+        if (command == 'skip') {
+            if (!vc) {
+                message.reply('You gotta be in a voice channel bro');
+                return;
+            } else if (songArray == 0) {
+                message.channel.send('There are no other songs to skip');
+            } else {
+                connection.dispatcher.end();
+            }
         }
     }
 }
