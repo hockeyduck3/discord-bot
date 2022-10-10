1c9ca2b177f7f2f2520542c942bdd072fe22beff
LJ <hockeyduck3@gmail.com>
2022-10-10 16:42:53 -0600

    Added the remove functions and got rid of the old functions

--- old functions/music functions/remove.js
+++
@@ -1,9 +0,0 @@
-module.exports = function remove(guild, arg) {
-    let songName = guild.songArray[arg-1].title;
-
-    guild.songArray.splice(arg-1, 1);
-
-    guild.text.send(`${songName} has been removed from the queue`);
-
-    return;
-}
