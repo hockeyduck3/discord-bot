c5177750dc74304e441f91a04a23789d31cf8652
LJ <hockeyduck3@gmail.com>
2022-10-11 17:36:02 -0600

    Made a small change to the leave function in play.js and updated the index.js file for prime time

--- commands/music functions/play.js
+++ commands/music functions/play.js
@@ -215,7 +215,17 @@ module.exports = {
         function leaveTimer(server, guildId) {
             setTimeout(() => {
                 if (server.audioStatus == 'stopped') {
-                    server.text.send('Looks like there haven\'t been anymore song requests in a minute, so I\'m gonna head out. ✌️')
+                    const leaveEmbed = new EmbedBuilder()
+                            .setColor([2, 12, 25])
+                            .setAuthor({
+                                name: 'Tilly Music Player',
+                                iconURL: 'https://i.pinimg.com/474x/80/3a/1f/803a1f2849f12dde465ab9143f50187e.jpg'
+                            })
+                            .setDescription('I haven\'t gotten any new song requests in a hot minute, so I\'m gonna head out. ✌️')
+
+                    server.text.send({
+                        embeds: [leaveEmbed]
+                    })
 
                     server.connection.destroy();
                     serverMap.delete(guildId);
